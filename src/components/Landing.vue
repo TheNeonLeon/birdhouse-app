b0046978e2bd66423ba895be9e5de6fd2b50d52d
Jensengbg-Henrik-Johansson <54744154+Rokakerman@users.noreply.github.com>
2020-11-03 15:27:54 +0100

    Basic css box/containers layout

---
+++ src/components/Landing.vue
@@ -0,0 +1,33 @@
+<template>
+  <header>
+      
+  </header>
+  <main>
+      <container>
+      </container>
+  </main>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style lang="scss" scoped>
+@import '../styles/base.sass';
+
+header {
+    border: 1px solid black;
+    grid-row: 5/7;
+    grid-column: 2/7;
+
+}
+
+main {
+    border: 1px solid red;
+    grid-row: 7/11;
+    grid-column: 2/7;
+}
+
+</style>
